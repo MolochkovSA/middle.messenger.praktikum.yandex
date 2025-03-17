73c15c17a03d204153487b296b2cf3c6264fd7a3
MolochkovSA <wwwsergwww@mail.ru>
2025-03-17 22:19:34 +0300

    fix: setObjectValues

--- src/utils/setObjectValues.ts
+++ src/utils/setObjectValues.ts
@@ -1,7 +1,7 @@
 import { Indexed } from '@/types'
 import { isObject } from '.'
 
-export function setObjectValues<T extends Indexed>(lhs: T, rhs: Indexed): T {
+export function setObjectValues<T extends Indexed>(lhs: T, rhs: DeepPartial<T>): T {
   const result: T = {} as T
 
   const keys = Object.keys(rhs)
@@ -9,8 +9,6 @@ export function setObjectValues<T extends Indexed>(lhs: T, rhs: Indexed): T {
   for (const key of keys) {
     const val1 = lhs[key]
     const val2 = rhs[key]
-
-    if (val1 === undefined) continue
 
     if (isObject(val1) && isObject(val2)) {
       ;(result as Indexed)[key] = setObjectValues(val1, val2)
