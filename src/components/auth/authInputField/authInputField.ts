fd37a45d4b4fbedca116bb8495f757f9d9d03f92
MolochkovSA <wwwsergwww@mail.ru>
2025-03-20 16:05:07 +0300

    fix: styled InputField

--- src/components/auth/authInputField/authInputField.ts
+++ src/components/auth/authInputField/authInputField.ts
@@ -1,28 +1,13 @@
 import { InputField } from '@/components'
 import { InputFieldProps } from '@/components/common/inputField'
-import { Block } from '@/core'
 
 import styles from './authInputField.module.scss'
 
-type AuthInputFieldChildren = {
-  InputField: InputField
-}
-
-export class AuthInputField extends Block<InputFieldProps, {}, AuthInputFieldChildren> {
-  constructor({ className, ...rest }: InputFieldProps) {
+export class AuthInputField extends InputField {
+  constructor(props: InputFieldProps) {
     super({
-      children: {
-        InputField: new InputField({ ...rest, className: `${styles.input} ${className ?? ''}`.trim() }),
-      },
+      ...props,
+      className: `${styles.input} ${props.className ?? ''}`.trim(),
     })
-  }
-
-  render(): string {
-    const props = this.getProps()
-    const { InputField } = this.getChildren()
-
-    InputField.setProps(props)
-
-    return `{{{ InputField }}}`
   }
 }
--- src/components/profile/profileInputField/profileInputField.ts
+++ src/components/profile/profileInputField/profileInputField.ts
@@ -1,29 +1,13 @@
 import { InputField } from '@/components'
 import { InputFieldProps } from '@/components/common/inputField'
-import { Block } from '@/core'
 
 import styles from './profileInputField.module.scss'
 
-type ProfileInputFieldChildren = {
-  InputField: InputField
-}
-
-export class ProfileInputField extends Block<InputFieldProps, {}, ProfileInputFieldChildren> {
-  constructor({ className, ...rest }: InputFieldProps) {
+export class ProfileInputField extends InputField {
+  constructor(props: InputFieldProps) {
     super({
-      props: { ...rest },
-      children: {
-        InputField: new InputField({ ...rest, className: `${styles.profileInput} ${className ?? ''}`.trim() }),
-      },
+      ...props,
+      className: `${styles.input} ${props.className ?? ''}`.trim(),
     })
-  }
-
-  render(): string {
-    const { value, disabled } = this.getProps()
-    const { InputField } = this.getChildren()
-
-    InputField.setProps({ value, disabled })
-
-    return `{{{ InputField }}}`
   }
 }
